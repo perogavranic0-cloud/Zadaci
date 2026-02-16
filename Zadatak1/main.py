2d417f96593c8e61786bfb2142e1bfa226a5c90b
perogavranic0-cloud <perogavranic0@gmail.com>
2026-02-16 14:16:42 +0100

    Update main.py

--- Zadatak1/main.py
+++ Zadatak1/main.py
@@ -1,1 +1,9 @@
+# Zadatak 1: Najmanji i najveći decimalni broj.
 
+## Opis zadatka:
+
+Program najprije učitava cijeli broj  N
+Nakon toga učitava decimalne brojeve dok se ne unese točno N brojeva koji su:
+
+-veći od 0
+-manji od 100
