c8d1cc30e0e4a8d7f34087e438a6ab02887de297
Pero <perogavranic0@gmail.com>
2026-02-16 18:04:11 +0100

    Add divisibility checks for user input numbers

--- Zadatak2/Zadatak2.py
+++ Zadatak2/Zadatak2.py
@@ -1,1 +1,10 @@
-
+for N in range(10):
+    N = int(input("Napisi N broj: "))
+    if N % 3 == 0 and N % 4 == 0:
+        print("Broj je dijeljiv sa 3 i 4")
+    elif N % 3 == 0:
+        print("Broj je djeljiv sa 3")
+    elif N % 4 == 0:
+        print("Broj je djeljiv sa 4")
+    else:
+        print("Čudan ti je broj")
