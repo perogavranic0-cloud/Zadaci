9ed1068250b53e1430b4f4fa7c504a6a30fa044a
Pero <perogavranic0@gmail.com>
2026-02-17 22:04:15 +0100

    Add functionality to track second last input number
    Implement logic to read integers until N is entered and print the second last number.

--- Zadatak4/Zadatak4.py
+++ Zadatak4/Zadatak4.py
@@ -1,1 +1,14 @@
+#Učitajte cijeli broj N. Nakon toga učitavajte cijele brojeve tako dugo dok opet ne učitate broj N. Ispišite koji je predzadnji broj bio učitan (broj prije broja N)
 
+N = int(input("Napisi N broj: "))
+
+prethodni = None
+
+while True:
+    X = int(input("Upisi cijeli broj: "))
+
+    if X == N:
+        print(prethodni)
+        break
+
+    prethodni = X
