63a1ad83aec22f16d537ee41fb9371417276d32b
Pero <perogavranic0@gmail.com>
2026-02-19 22:46:16 +0100

    Implement product calculation for every third number
    Add functionality to calculate the product of every third input number, handling zeros and negatives.

--- Zadatak5/Zadatak5.py
+++ Zadatak5/Zadatak5.py
@@ -1,1 +1,17 @@
+#Učitajte 12 brojeva. Izračunajte umnožak svakog trećeg učitanog broja. Ako učitate broj 0 ispišite „Slijed nije pravilan” i
+#završite program. Ako učitate negativni broj, za izračun umnoška uzmite njegovu apsolutnu vrijednost. Ako niti jedan učitani broj nije bio 0, na kraju ispišite umnožak.
 
+
+umnozak = 1
+
+for i in range(1, 13):
+    x = int(input("Upisi broj: "))
+
+    if x == 0:
+        print("Slijed nije pravilan")
+        break
+
+    if i % 3 == 0:
+        umnozak *= abs(x)
+else:
+    print("Umnozak je", umnozak)
